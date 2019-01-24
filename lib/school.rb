be48e7a47a2c5e420fe5de3ee64662bf98b5a35b
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-24 18:58:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+   def add_student(student_name, grade)
+     @student_name = student_name
+     @grade = grade
+
+     @roster[@grade] = @student_name = AC Slater
+   end
+     
+    
+end
